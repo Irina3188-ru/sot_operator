8507a87ae0120de46df9db4ca936dc5c2187c522
Нетология_Ирина <cherry.liz19702@gmail.com>
2025-07-18 22:58:11 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int initial_balance = 100;
-        int top_up_balance = 1560;
-        int one_rub_bonus = 100;
+        int initialBalance = 100;
+        int topUpBalance = 1560;
+        int oneRubBonus = 100;
 
         int bonus;
-        if (top_up_balance > 1_000)
-        {
-            bonus = top_up_balance / one_rub_bonus;
+        if (topUpBalance > 1_000) {
+            bonus = topUpBalance / oneRubBonus;
         } else {
-            bonus  = 0;}
-        int balance = initial_balance + top_up_balance + bonus;
-            System.out.println(balance);
-            System.out.println(bonus);
+            bonus = 0;
         }
+        int balance = initialBalance + topUpBalance + bonus;
+        System.out.println(balance);
+        System.out.println(bonus);
     }
+}
